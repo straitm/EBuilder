d8540d9a1103686018c222ff326d801e42924a40
Matthew Strait <strait@physics.umn.edu>
2018-01-03 17:09:32 -0600

    Simplify file handling.
    * Set permissions to 644 and 755, not 666 and 777.
    
    * Move GetDir into EventBuilder.cxx and remove unused option.
    
    * Drop concept of DAQ disk number.  We will use directory names.
    
    * Don't avoid a few directory listings at the cost having a global
    variable which is mutated by multiple functions.  Just list the
    directory when we need to.
    
    * Improve some print statements.
    
    * Remove checks that can't fail about whether the file names that
    we just sorted into place to match up with the USB numbers are
    matched up with the USB numbers.
    
    * Change option -H to mean the input directory.
    
    * Do not create the unused output directory /processed.
    
    * Reduce the number of references to RunNumber.  I'm not sure how
    run number will be handled and it may be removed.
    
    * Put processed files in a subdirectory of the input directory
    called /decoded rather than a sibling directory.  Now we don't
    need to make assumptions about the path of the input directory.

--- include/USBstreamUtils.h
+++ include/USBstreamUtils.h
@@ -17,18 +17,4 @@ bool LessThan(const std::vector<int32_t> & lhs,
 
 void start_log();
 
-// Fills myfiles with a list of files in the given directory.
-//
-// These files are the set that does not have a dot in their name or, if 'allowdots',
-// are at least three characters long (I guess to exclude the . and ..
-// directories?).
-//
-// Also exclude files with names containing "baseline" or "processed" unless
-// 'allow_bl_and_pc' is true.
-//
-// Return true if no files are found that satisfy those rules, including if
-// the directory couldn't be read.  Otherwise, returns false.
-bool GetDir(const std::string dir, std::vector<std::string> &myfiles,
-            const bool allowdots = false, const bool allow_bl_and_pc = false);
-
 #endif
